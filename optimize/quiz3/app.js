6d1eb69844ea6ca733c350841366cc055ec6fcd6
ServicedevPubl\Servicedev_Publ <hjshin@playwith.co.kr>
2018-02-01 14:28:40 +0900

    3주차 과제

--- optimize/quiz3/app.js
+++ optimize/quiz3/app.js
@@ -1,1 +1,5 @@
-console.log("We are almost done with Browser Critical Rendering Optimization");
+function load(){
+  console.log("We are almost done with Browser Critical Rendering Optimization");
+};
+
+window.onload = load;
